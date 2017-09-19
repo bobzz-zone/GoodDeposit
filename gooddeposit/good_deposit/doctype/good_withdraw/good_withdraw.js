6310cb4576efc5d40f8478fc1b27d7a96ed2221f
bobzz-zone <bobzz.zone@gmail.com>
2017-09-20 02:07:36 +0700

    a

--- gooddeposit/good_deposit/doctype/good_withdraw/good_withdraw.js
+++ gooddeposit/good_deposit/doctype/good_withdraw/good_withdraw.js
@@ -4,19 +4,23 @@ cur_frm.add_fetch("item_code","item_name","item_name");
 cur_frm.add_fetch("item_code","description","description");
 frappe.ui.form.on('Good Withdraw', {
 	refresh: function(frm) {
+		var me=this;
 		if (frm.doc.docstatus===0) {
 				cur_frm.add_custom_button(__('Good Deposit'),
 					function() {
 						erpnext.utils.map_current_doc({
 							method: "gooddeposit.good_deposit.doctype.good_deposit.good_deposit.make_withdrawal",
 							source_doctype: "Good Deposit",
+							target: me.frm,
+							date_field: "posting_date",
+							setters: {},
 							get_query_filters: {
 								docstatus: 1,
-								status: "Deposited",
 								company: cur_frm.doc.company
 							}
 						})
 					}, __("Get items from"));
+				
 			}
 	}
 });
