47b35fbc01c56284fc17835539558948872ed551
bobzz-zone <bobzz.zone@gmail.com>
2017-10-03 16:06:53 +0700

    asd

--- gooddeposit/good_deposit/report/customer_good_deposit_balance/customer_good_deposit_balance.py
+++ gooddeposit/good_deposit/report/customer_good_deposit_balance/customer_good_deposit_balance.py
@@ -2,11 +2,11 @@ from __future__ import unicode_literals
 import frappe
 
 def execute(filters=None):
-	columns, data = ["Item Code::200","Item Name::200","Description::200","Qty:Float:100"], []
+	columns, data = ["Customer::200","Item Code::200","Item Name::200","Description::200","Qty:Float:100"], []
 	where = ""
 	if filters.get("customer"):
 		where = """ and p.customer = "{}" """.format(filters.get("customer"))
-	data = frappe.db.sql("""select d.item_code,d.item_name,d.description,sum(d.qty-d.withdrawed) as "total" 
+	data = frappe.db.sql("""select p.customer,d.item_code,d.item_name,d.description,sum(d.qty-d.withdrawed) as "total" 
 		from `tabGood Deposit Item` d join `tabGood Deposit` p on d.parent=p.name
 		where d.qty>d.withdrawed {} group by d.item_code""".format(where),as_list=1)
 	return columns, data
